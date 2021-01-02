3932af911a23d120f25ab45c33fa6704f2257065
jiuduweiliangeer <64030931@qq.com>
2021-01-02 15:08:32 +0800

    demo

--- src/main/java/com/example/internet/demo/bean/Person.java
+++ src/main/java/com/example/internet/demo/bean/Person.java
@@ -8,13 +8,30 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Map;
+/*
+ * 只有这个组件是容器的组件，才能使用容器提供的@ConfigurationProperties功能；
+ * @ConfigurationProperties默认从全局配置文件中获取值；
+ * 如果想要读取指定配置文件，则使用@PropertySouce来读取；
+ *
+ *
+ * */
 @Component
 /*@PropertySource(value = {"classpath:person.properties"})*/
-@ConfigurationProperties(prefix = "person")
+@ConfigurationProperties(prefix = "person")//将配置文件加载到这个bean中，这个bean必须是组件，所以需要加入Component注解
 /*@ConfigurationProperties(prefix = "person")*/
 public class Person {
+    /*
+       * <bean class='person'>
+           <property name="lastName" value="字面量/${key}从环境变量，配置文件中获取值/#{SpEL}"></property>
+       * </bean>
+       *
+       *
+       * */
+    //@Value("${person.lastName}")//从配置文件中取值
     private String name;
+    //@Value("#{11*22}")//直接计算式计算
     private Integer age;
+    //@Value("true")//直接给字面量
     private boolean boss;
     private Map<String,Object> map;
     private List<Object> list;
